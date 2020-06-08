66d664a8cc2be870a96a0653d049c28677eb88b2
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-06-08 20:36:44 +0300

    Signed user out with Firebase and logout button

--- app/src/main/java/com/example/authenticationapp/MainActivity.java
+++ app/src/main/java/com/example/authenticationapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.authenticationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view) {
+//        Create a Firebase instance
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+//        Log the user out
+        firebaseAuth.signOut();
+//        Start the Login activity
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
